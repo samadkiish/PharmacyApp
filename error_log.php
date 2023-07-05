17db69aed0e12cd8f14bab61d0ce90d1baa7d0ec
Abdisamad Ali Mohamed <kiish@just.edu.so>
2023-07-05 21:46:26 +0300

    error log

---
+++ error_log.php
@@ -0,0 +1,14 @@
+<?php
+
+function checkForErrors() {
+    $lastError = error_get_last();
+
+    if ($lastError !== null && $lastError['type'] === E_ERROR) {
+        // Handle the error
+        echo "An error occurred: " . $lastError['message'];
+    } else {
+        echo "No errors found.";
+    }
+}
+
+checkForErrors();
